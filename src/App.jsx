57c2266d8c93ac8cc547c044f1f6575db2f31e94
st0ckitch <135453997+st0ckitch@users.noreply.github.com>
2025-01-28 02:14:45 +0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GameBoard from './components/GameBoard';
+
+const App = () => {
+  return (
+    <div className="App">
+      <GameBoard />
+    </div>
+  );
+};
+
+export default App;
