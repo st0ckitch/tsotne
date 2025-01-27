8f0d3bf61cddc10b372cb8a4b7ae185eac018363
st0ckitch <135453997+st0ckitch@users.noreply.github.com>
2025-01-28 03:28:50 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as THREE from 'three';
-
-// Configure THREE.js
-THREE.ColorManagement.enabled = false;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
