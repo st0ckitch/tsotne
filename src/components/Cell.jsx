254f8b5b0278fa1875635c09d86a624523384aba
st0ckitch <135453997+st0ckitch@users.noreply.github.com>
2025-01-28 03:20:36 +0400

    Update Cell.jsx

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,42 +1,53 @@
 import React from 'react';
-import { Text } from '@react-three/drei';
 
-const Cell = ({ position, cellData, isPlayerHere, isBotHere }) => {
+const Cell = ({ position, cell, isPlayerHere, isBotHere }) => {
   const getCellColor = () => {
-    switch (cellData.type) {
+    switch (cell.type) {
       case 'buff':
-        return '#4ade80';
+        return '#68D391';  // green
       case 'debuff':
-        return '#ef4444';
+        return '#F56565';  // red
       case 'goblin':
-        return '#eab308';
+        return '#F6AD55';  // orange
       default:
-        return '#6b7280';
+        return '#4A5568';  // gray
     }
   };
 
   return (
     <group position={position}>
-      {/* Base hexagon */}
-      <mesh position={[0, -0.1, 0]} receiveShadow>
-        <cylinderGeometry args={[1, 1, 0.2, 6]} />
+      {/* Base cell */}
+      <mesh position={[0, -0.1, 0]}>
+        <boxGeometry args={[1.8, 0.2, 1.8]} />
         <meshStandardMaterial color={getCellColor()} />
       </mesh>
 
-      {/* Cell number */}
-      <Text
-        position={[0, 0.2, 0]}
-        rotation={[-Math.PI / 2, 0, 0]}
-        fontSize={0.5}
-        color="white"
-      >
-        {cellData.id.toString()}
-      </Text>
+      {/* Player marker */}
+      {isPlayerHere && (
+        <mesh position={[0.5, 0.3, 0]}>
+          <sphereGeometry args={[0.3]} />
+          <meshStandardMaterial color="#4299E1" />
+        </mesh>
+      )}
 
-      {/* Effect indicator */}
-      {cellData.type !== 'normal' && (
+      {/* Bot marker */}
+      {isBotHere && (
+        <mesh position={[-0.5, 0.3, 0]}>
+          <sphereGeometry args={[0.3]} />
+          <meshStandardMaterial color="#F56565" />
+        </mesh>
+      )}
+
+      {/* Cell number */}
+      <mesh position={[0, 0, 0]}>
+        <textGeometry args={[cell.id.toString(), { size: 0.5, height: 0.1 }]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
+
+      {/* Effect indicator for special cells */}
+      {cell.type !== 'normal' && (
         <mesh position={[0, 0.2, 0]}>
-          <sphereGeometry args={[0.2]} />
+          <boxGeometry args={[0.3, 0.3, 0.3]} />
           <meshStandardMaterial color={getCellColor()} emissive={getCellColor()} emissiveIntensity={0.5} />
         </mesh>
       )}
